c08369a233ac80a1bfc3071a502ba4cea21e42c9
hamzaplojovic <hamzaplojovic9@gmail.com>
2022-09-19 14:58:35 +0200

    fully responsive cards

--- src/components/Home/Users/UsersComponents/UsersItemStyles.tsx
+++ src/components/Home/Users/UsersComponents/UsersItemStyles.tsx
@@ -2,11 +2,19 @@ import { createStyles } from "@mantine/core";
 
 export const useStyles = createStyles((theme) => ({
     item: {
+        width: "30%",
         backgroundColor:
             theme.colorScheme === "dark" ? theme.colors.dark[7] : theme.white,
         padding: theme.spacing.lg,
         borderRadius: theme.radius.md,
         margin: "2% 0%",
+        [`@media (max-width: 1175px)`]: {
+            width: "45%",
+        },
+        [`@media (max-width: 850px)`]: {
+            width: "100%",
+            margin: "5% 0%",
+        },
     },
     avatar: {
         borderRadius: 120,

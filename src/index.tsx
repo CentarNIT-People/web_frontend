9b1c77980590cebe72b5f6c74738170d560246e1
Ajsa Besirovic <besirovicajsa@gmail.com>
2022-09-22 16:53:24 +0200

    minor changes

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 const root = ReactDOM.createRoot(
-    document.getElementById("root") as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
-    <React.StrictMode>
-        <h1>HI</h1>
-    </React.StrictMode>
+  <React.StrictMode>
+    <h1>HI</h1>
+  </React.StrictMode>
 );

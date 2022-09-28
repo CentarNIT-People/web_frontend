ca2d205637a3ce3c298b434390bd12406dab0382
hamzaplojovic <hamzaplojovic9@gmail.com>
2022-09-28 14:15:51 +0200

    tweaking

--- src/components/Home/Users/FullPageComponents/FullPage.tsx
+++ src/components/Home/Users/FullPageComponents/FullPage.tsx
@@ -1,7 +1,12 @@
+import { Image } from "@mantine/core";
 import { useLocation } from "react-router-dom";
 
 export const FullPage = () => {
     const location = useLocation();
-    console.log(location.state.item);
-    return <h1>Full Page</h1>;
+    return (
+        <div>
+            <h1>{location.state.item.full_name}</h1>
+            <Image src={location.state.item.avatar} width={180} />
+        </div>
+    );
 };

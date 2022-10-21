d610cfee05fe380e2fcce9bb2c296cc49023fdda
kacarervin <kacarervin@gmail.com>
2022-10-21 16:25:51 +0200

    Add users

--- src/components/Users/HomeStyles.tsx
+++ src/components/Users/HomeStyles.tsx
@@ -1,15 +1,61 @@
 import { createStyles } from "@mantine/core";
 
 export const useStyles = createStyles((theme, _params, getRef) => ({
-    home: {
-        display: "flex",
-        flexDirection: "column",
-        width: "100%",
+  home: {
+    display: "flex",
+    flexDirection: "column",
+    width: "100%",
+    padding: "30px",
+    alignItems: "flex-end",
+  },
+  app: {
+    display: "flex",
+    [`@media (max-width: 1000px)`]: {
+      flexDirection: "column",
     },
-    app: {
-        display: "flex",
-        [`@media (max-width: 1000px)`]: {
-            flexDirection: "column",
-        },
+  },
+  searchWrapper: {
+    width: "36%",
+    marginBottom: "30px",
+    marginRight: "50px",
+  },
+  avatarImg: {
+    width: "50px",
+    height: "50px",
+    borderRadius: "50%",
+  },
+  userRow: {
+    ":hover": {
+      background: "whitesmoke",
     },
+    height: "60px",
+  },
+  table: {
+    width: "40%",
+    display: "inline-block",
+  },
+  wrapper: {
+    width: "100%",
+    display: "flex",
+    justifyContent: "space-between",
+  },
+  selectedUser: {
+    width: "40%",
+    height: "500px",
+    marginLeft: "20px",
+    borderRadius: "13px",
+    boxShadow: "1px 1px 10px 3px whitesmoke",
+    padding: "25px",
+  },
+  selectedAvatar: {
+    width: "100px",
+    height: "100px",
+    borderRadius: "50%",
+    marginLeft: "10px",
+  },
+  selectedFullname: {
+    display: "inline-block",
+    marginLeft: "70px",
+    fontSize: "25px",
+  },
 }));
--- src/components/Users/UsersHome.tsx
+++ src/components/Users/UsersHome.tsx
@@ -1,13 +1,90 @@
-import { Hero } from "./Hero/Hero";
+import { Input, Table } from "@mantine/core";
+import { useEffect, useState } from "react";
+import api from "../../api";
 import { useStyles } from "./HomeStyles";
-import { Users } from "./Users/Users";
 
 export const UsersHome = () => {
-    const { classes } = useStyles();
-    return (
-        <div className={classes.home}>
-            <Hero />
-            <Users />
-        </div>
+  const { classes } = useStyles();
+  const [users, setUsers] = useState([]);
+  const [filteredUsers, setFilteredUsers] = useState([]);
+  const [searchValue, setSearchValue] = useState("");
+  const [selectedUser, setSelctedUser] = useState(null);
+  useEffect(() => {
+    api.getUsers().then((res) => {
+      setFilteredUsers(res);
+      setUsers(res);
+    });
+  }, []);
+
+  useEffect(() => {
+    const searchedUser = users.filter(
+      (user) =>
+        user?.full_name
+          ?.toLowerCase()
+          ?.startsWith(searchValue?.toLowerCase()) ||
+        user?.email?.toLowerCase()?.startsWith(searchValue?.toLowerCase())
     );
+    setFilteredUsers(searchedUser);
+  }, [searchValue]);
+
+  const rows = filteredUsers.map((user) => (
+    <tr
+      key={user.firstName}
+      className={classes.userRow}
+      style={{
+        background: selectedUser?.full_name === user?.full_name && "whitesmoke",
+      }}
+      onClick={() => setSelctedUser(user)}
+    >
+      <td>
+        <img src={user.avatar} className={classes.avatarImg} />
+      </td>
+      <td>{user.full_name}</td>
+      <td>{user.email}</td>
+    </tr>
+  ));
+
+  return (
+    <div className={classes.home}>
+      <div className={classes.searchWrapper}>
+        <Input
+          placeholder="Search users"
+          radius="md"
+          size="md"
+          value={searchValue}
+          onChange={(e) => setSearchValue(e.target.value)}
+        />
+      </div>
+      <div className={classes.wrapper}>
+        <div className={classes.selectedUser}>
+          <img src={selectedUser?.avatar} className={classes.selectedAvatar} />
+          <h2 className={classes.selectedFullname}>
+            {selectedUser?.full_name}
+          </h2>
+          <h3 style={{ textAlign: "center", margin: "35px 0 " }}>
+            {selectedUser?.job}
+          </h3>
+          <h3 style={{ textAlign: "center", margin: "35px 0 " }}>
+            {selectedUser?.phone_number}
+          </h3>
+          <h3 style={{ textAlign: "center", margin: "35px 0 " }}>
+            {selectedUser?.email}
+          </h3>
+          <h3 style={{ textAlign: "center", margin: "35px 0 " }}>
+            {selectedUser?.github}
+          </h3>
+        </div>
+        <Table className={classes.table}>
+          <thead>
+            <tr>
+              <th>Image</th>
+              <th>Fullname</th>
+              <th>Email</th>
+            </tr>
+          </thead>
+          <tbody>{rows}</tbody>
+        </Table>
+      </div>
+    </div>
+  );
 };

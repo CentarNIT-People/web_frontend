e609b56cf6666e7303768fd5358fc2cb7d7aaf39
hamzaplojovic <hamzaplojovic9@gmail.com>
2022-12-04 12:57:13 +0100

    fixed deepsource issues

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,7 @@
-import { HeaderSimple } from "./HeaderComponents/HeaderSimple";
+import React from "react";
 import { data } from "./HeaderComponents/HeaderData";
+import { HeaderSimple } from "./HeaderComponents/HeaderSimple";
+
 
 export const Header = () => {
     return <HeaderSimple links={data} />;
